96fe80ecfb0840afa90cf1222607225c51dc9c6d
Abrantie <as.cyril3@gmail.com>
2025-07-12 14:28:07 +0000

    Ternary Operations

---
+++ Tutorials/Ternary operations.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* int score;
+    cout << "Enter your score: ";
+    cin >> score;
+
+    (score >= 0 && score <= 100) ?
+        (score >= 80 ? cout << "Grade A" 
+                    : score >= 70 ? cout << "Grade B" 
+                    : score >= 60 ? cout << "Grade C" 
+                    : score >= 50 ? cout << "Grade D" 
+                    : cout << "Grade F")
+    : cout << "Invalid score";
+    */
+
+    // float num1, num2, num3;
+    // cout << "Enter three numbers separated by spaces: ";
+    // cin >> num1 >> num2 >> num3;
+
+    // (num1 <= num2 && num1 <= num3) ? cout << "The smallest number is " << num1 <<"\n"
+    //                             : (num2 <= num1 && num2 <= num3) ? cout << "The smallest number is " << num2 <<"\n"
+    //                             : cout << "The smallest number is " << num3 <<"\n";
+
+    // (num1 >= num2 && num1 >= num3) ? cout << "The largest number is " << num1 <<"\n"
+    //                             : (num2 >= num1 && num2 >= num3) ? cout << "The largest number is " << num2 <<"\n"
+    //                             : cout << "The largest number is " << num3 <<"\n";
+
+    // float number;
+    // cout << "Enter a number: ";
+    // cin >> number;
+    // (number < 0 ) ? cout << "Negtive\n"
+    //                 : number > 0 ? cout << " Positive\n"
+    //                 : cout << "Zero\n";
+
+
+
+    return 0;
+}
