1eed9573b7f34fc81cad1290af806da23df0b9d2
Abrantie <as.cyril3@gmail.com>
2025-07-12 14:43:14 +0000

    Uncomment code for score evaluation and number comparisons

--- Tutorials/Ternary operations.cpp
+++ Tutorials/Ternary operations.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    /* int score;
+    int score;
     cout << "Enter your score: ";
     cin >> score;
 
@@ -13,26 +13,26 @@ int main() {
                     : score >= 50 ? cout << "Grade D" 
                     : cout << "Grade F")
     : cout << "Invalid score";
-    */
+    
 
-    // float num1, num2, num3;
-    // cout << "Enter three numbers separated by spaces: ";
-    // cin >> num1 >> num2 >> num3;
+    float num1, num2, num3;
+    cout << "Enter three numbers separated by spaces: ";
+    cin >> num1 >> num2 >> num3;
 
-    // (num1 <= num2 && num1 <= num3) ? cout << "The smallest number is " << num1 <<"\n"
-    //                             : (num2 <= num1 && num2 <= num3) ? cout << "The smallest number is " << num2 <<"\n"
-    //                             : cout << "The smallest number is " << num3 <<"\n";
+    (num1 <= num2 && num1 <= num3) ? cout << "The smallest number is " << num1 <<"\n"
+                                : (num2 <= num1 && num2 <= num3) ? cout << "The smallest number is " << num2 <<"\n"
+                                : cout << "The smallest number is " << num3 <<"\n";
 
-    // (num1 >= num2 && num1 >= num3) ? cout << "The largest number is " << num1 <<"\n"
-    //                             : (num2 >= num1 && num2 >= num3) ? cout << "The largest number is " << num2 <<"\n"
-    //                             : cout << "The largest number is " << num3 <<"\n";
+    (num1 >= num2 && num1 >= num3) ? cout << "The largest number is " << num1 <<"\n"
+                                : (num2 >= num1 && num2 >= num3) ? cout << "The largest number is " << num2 <<"\n"
+                                : cout << "The largest number is " << num3 <<"\n";
 
-    // float number;
-    // cout << "Enter a number: ";
-    // cin >> number;
-    // (number < 0 ) ? cout << "Negtive\n"
-    //                 : number > 0 ? cout << " Positive\n"
-    //                 : cout << "Zero\n";
+    float number;
+    cout << "Enter a number: ";
+    cin >> number;
+    (number < 0 ) ? cout << "Negtive\n"
+                    : number > 0 ? cout << " Positive\n"
+                    : cout << "Zero\n";
 
 
 
